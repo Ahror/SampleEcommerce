863ec5321d829431f3a23ec3d2a9f4c56f66ec2e
Ahror Kayumov <ahrorbek_n@hotmail.com>
2020-08-24 22:30:27 +0500

    Refactoring

--- src/Frontend/SampleEcommerce.Moblie/SampleEcommerce.Moblie/SampleEcommerce.Moblie/DependencyInitializerCore.cs
+++ src/Frontend/SampleEcommerce.Moblie/SampleEcommerce.Moblie/SampleEcommerce.Moblie/DependencyInitializerCore.cs
@@ -3,13 +3,12 @@ using ReactiveUI;
 using SampleEcommerce.Mobile.Abstractions;
 using SampleEcommerce.Mobile.Helper;
 using SampleEcommerce.Mobile.Services;
-using SampleEcommerce.Mobile.Services.Cart;
 using SampleEcommerce.Mobile.ViewModels;
 using SampleEcommerce.Mobile.Views;
 
 namespace SampleEcommerce.Mobile
 {
-    public class DependencyInitializerCore
+    public abstract class DependencyInitializerCore
     {
         public static IContainer Container { get; private set; }
 
@@ -23,13 +22,15 @@ namespace SampleEcommerce.Mobile
             builder.RegisterType<HomePage>().As<IViewFor<HomeViewModel>>();
 
             builder.RegisterType<AppSettings>().As<IAppSettings>().SingleInstance();
-            builder.RegisterType<CartService>().As<ICartService>().SingleInstance();
             builder.RegisterType<DialogService>().As<IDialogService>().SingleInstance();
             builder.RegisterType<NavigationService>().As<INavigationService>().SingleInstance();
             builder.RegisterType<AuthenticationService>().As<IAuthenticationService>().SingleInstance();
 
+            RegisterTypes(builder);
             return Container = builder.Build();
         }
+
+        protected abstract void RegisterTypes(ContainerBuilder builder);
     }
 
 }
