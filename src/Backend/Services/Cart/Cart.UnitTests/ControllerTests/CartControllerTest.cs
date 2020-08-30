200b8ef65aba466b0c6cd478a5f00cb589357bd2
Ahror Kayumov <ahrorbek_n@hotmail.com>
2020-08-30 09:56:59 +0500

    Controllers added

---
+++ src/Backend/Services/Cart/Cart.UnitTests/ControllerTests/CartControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using Cart.Api.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xunit;
+
+namespace Cart.UnitTests.ControllerTests
+{
+    public class CartControllerTest : BaseAutoMockedTest<CartController>
+    {
+        [Fact]
+        public void Get_should_return_foods()
+        {
+            // Given
+            var foods = Enumerable.Repeat(new Food(), 5);
+            var foodDtos = Enumerable.Repeat(new FoodDto(), 5);
+
+            GetMock<IRepository<Food>>().Setup(x => x.GetAll()).Returns(foods.AsQueryable());
+            GetMock<IMapper>().Setup(x => x.Map<IEnumerable<FoodDto>>(It.IsAny<List<Food>>())).Returns(foodDtos);
+
+            // When
+            var result = ClassUnderTest.Get(10, 6);
+
+            // Then
+            result.Should().Equal(foodDtos);
+        }
+    }
+}
